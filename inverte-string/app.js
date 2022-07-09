36284b8ec51dedcb8fad100e6f2000ed2d338535
Guilherme Ramos <guilhermeramos31.gr@gmail.com>
2022-07-08 23:21:03 -0300

    feat-inverte-string

--- inverte-string/app.js
+++ inverte-string/app.js
@@ -1,1 +1,9 @@
-
+const string = "target sistemas";
+console.log(reverse(string));
+function reverse(string) {
+	var gnirts = [];
+	for (let i = string.length; i > 0; i--) {
+		gnirts.push(string[i - 1]);
+	}
+	return gnirts.toString().replace(/[',']/g, "");
+}
